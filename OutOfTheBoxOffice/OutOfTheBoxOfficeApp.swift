db0d0b6e8e8b30faba0aa8283d621497a395d8be
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-06-11 00:02:22 -0700

    Initial Commit

---
+++ OutOfTheBoxOffice/OutOfTheBoxOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutOfTheBoxOfficeApp.swift
+//  OutOfTheBoxOffice
+//
+//  Created by Suhail Khan on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct OutOfTheBoxOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
